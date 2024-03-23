d825695e1509e08bbf61bd58a01e583b9d013d98
Avondeil <123551851+Avondeil@users.noreply.github.com>
2024-03-23 12:40:45 +0500

    Prakt9.cpp

--- Prakt9/Prakt9.cpp
+++ Prakt9/Prakt9.cpp
@@ -1,59 +1,52 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <windows.h>
 
 using namespace std;
 
-
-
 // 24 Разработать многопоточную программу для аппроксимации данных методом наименьших квадратов.
 void Approximation(const vector<double>& x, const vector<double>& y, double& a, double& b) {
     double sum_x = 0, sum_y = 0, sum_xy = 0, squarSumX = 0;
 
-    // Вычисление сумм
     for (int i = 0; i < x.size(); ++i) {
         sum_x += x[i];
         sum_y += y[i];
         sum_xy += x[i] * y[i];
         squarSumX += x[i] * x[i];
     }
-    a = (sum_xy - sum_x * sum_y / x.size()) / (squarSumX - sum_x * sum_x / x.size());
-    b = (sum_y - a * sum_x) / x.size();
+    b = (sum_xy - sum_x * sum_y / x.size()) / (squarSumX - sum_x * sum_x / x.size());
+    a = (sum_y - b * sum_x) / x.size();
 }
 
-DWORD WINAPI Add(LPVOID nul)
+DWORD WINAPI Calculation(LPVOID vecPair)
 {
-    setlocale(LC_ALL, "ru");
-    vector<double> x = { 1, 2, 3, 4, 5 }; 
-    vector<double> y = { 2, 3, 4, 5, 6 }; 
-
+    pair<vector<double>, vector<double>>* par = reinterpret_cast<pair<vector<double>, vector<double>>*>(vecPair);
+    vector<double> x = par->first;
+    vector<double> y = par->second;
     double a, b;
     Approximation(x, y, a, b);
-    cout << "Исходные данные x: ";
-    for (const auto x : x)
-    {
-        cout << x << " ";
-    }
-    cout << endl << "Исходные данные y: ";
-    for (const auto y : y)
-    {
-        cout << y << " ";
-    }
-    cout << endl << "Результат: a = " << a << ", b = " << b << endl;
+    cout <<"a = " << a << " b = " << b << endl;
 
     return 0;
 }
 
 int main()
 {
-    HANDLE hThread;
-    DWORD IDThread;
-    hThread = CreateThread(NULL, 0, Add, NULL, 0, &IDThread);
-    if (hThread == NULL)
-    {
-        return GetLastError();
-    }
-    WaitForSingleObject(hThread, INFINITE);
-    CloseHandle(hThread);
+    setlocale(LC_ALL, "ru");
+    vector<double> x_1 = { 1, 2, 3, 4, 5 };
+    vector<double> y_1 = { 2, 3, 4, 5, 6 };
+
+    vector<double> x_2 = { 1, 2, 3, 4, 5 };
+    vector<double> y_2 = { 5, 15, 25, 45, 65 };
+
+    pair<vector<double>, vector<double>> first(x_1, y_1);
+    pair<vector<double>, vector<double>> second(x_2, y_2);
+    HANDLE hThread1 = CreateThread(NULL, 0, Calculation, &first, 0, NULL);
+    HANDLE hThread2 = CreateThread(NULL, 0, Calculation, &second, 0, NULL);
+    WaitForSingleObject(hThread1, INFINITE);
+    WaitForSingleObject(hThread2, INFINITE);
+    CloseHandle(hThread1);
+    CloseHandle(hThread2);
+
     return 0;
 }
